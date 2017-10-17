1684cf6463bd1683add3e7b027588e43aee79508
lpj24 <lipenju24@163.com>
2017-10-17 09:56:21 +0800

    'updatethread'

--- app.py
+++ app.py
@@ -1,60 +1,43 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-from flask import render_template
-from flask_sse import sse
+from flask import render_template, redirect, url_for
 from libs import coroutine
+from celery import Celery
 import threading
-import time
+import Queue
+from flask_socketio import SocketIO, emit, send
+import subprocess
 app = Flask(__name__)
-app.debug = False
-app.config['REDIS_URL'] = "redis://localhost"
-app.register_blueprint(sse, url_prefix='/stream')
+app.debug = True
+
+socket_io = SocketIO(app)
 
 
 @app.route('/')
 def hello_world():
-    f = open('C:\\Users\\Administrator\\Desktop\\error.log')
-    gen_calling_obj = printer()
-    monitor_file(f, gen_calling_obj)
-    # t = threading.Thread(target=log_file)
-    # t.start()
-    # t.join()
-
     return render_template('index.html')
 
 
-@coroutine
-def printer():
-    while 1:
-        line = yield
-        print line
-        sse.publish({'message': line}, type='greeting')
+@socket_io.on('show_log', namespace='/log')
+def show_log(msg):
+    print msg
+    r = subprocess.Popen("tail -f /home/huolibi/local/hbgj_statistics/log/error.log", stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, shell=True)
+    t = threading.Thread(target=tail_file, args=r)
+    t.start()
 
 
-def monitor_file(logfile, target):
-    logfile.seek(0, 2)
-    while 1:
-        line = logfile.readline()
-        if not line:
-            time.sleep(1)
-            continue
-        target.send(line)
+def tail_file(r):
+    line = r.readline()
+    while line:
+        emit('show_file_line', line, namespace='/log')
 
 
-def log_file():
-    f = open('C:\\Users\\Administrator\\Desktop\\error.log')
-    gen_calling_obj = printer()
-    monitor_file(f, gen_calling_obj)
 
 
-@app.route('/send')
-def send_file_line():
-    print 'send'
-    # f = open('error.log')
-    # gen_calling_obj = printer()
-    # monitor_file(f, gen_calling_obj)
-    # sse.publish({'message': 'lpj24'}, type='greeting')
-    return 'file msg send'
 
 if __name__ == '__main__':
-    app.run(port=8100, debug=app.debug, threaded=True)
+    socket_io.run(app, host='127.0.0.1', port=5000)
+    # f = open('C:\\Users\\Administrator\\Desktop\\error.log')
+    # gen_calling_obj = printer()
+    # monitor_file(f, gen_calling_obj)
