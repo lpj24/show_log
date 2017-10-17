33ed8ff4b5fa5a4611b60347a3a9d3184602c80c
lpj <pengjuli@gmail.com>
2017-10-16 20:04:00 -0700

    update

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from flask import Flask, request, copy_current_request_context
 from flask import render_template, redirect, url_for
 from libs import coroutine
-from celery import Celery
 import threading
 import Queue
 from flask_socketio import SocketIO, emit, send
@@ -20,17 +19,28 @@ def hello_world():
 
 @socket_io.on('show_log', namespace='/log')
 def show_log(msg):
-    r = subprocess.Popen("tail -f /home/huolibi/local/hbgj_statistics/log/error.log", stdin=subprocess.PIPE,
+    r = subprocess.Popen("tail -f ~/Downloads/error.log", stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE, shell=True)
-    t = threading.Thread(target=tail_file, args=(r, ))
+    # t = threading.Thread(target=tail_file, args=(r, ))
+    t = ThreadExecuteJob(r)
     t.start()
 
 
 def tail_file(r):
-    line = r.stdout.readline()
-    while line:
-        socket_io.emit('show_file_line', line, namespace='/log')
+    while r.stdout.readline():
+        socket_io.emit('show_file_line', r.stdout.readline() + '<br/>', namespace='/log')
 
+
+class ThreadExecuteJob(threading.Thread):
+    """Threaded Url Grab"""
+
+    def __init__(self, r):
+        threading.Thread.__init__(self)
+        self.r = r
+
+    def run(self):
+        while self.r.stdout.readline():
+            socket_io.emit('show_file_line', self.r.stdout.readline(), namespace='/log')
 
 
 
@@ -40,3 +50,6 @@ if __name__ == '__main__':
     # f = open('C:\\Users\\Administrator\\Desktop\\error.log')
     # gen_calling_obj = printer()
     # monitor_file(f, gen_calling_obj)
+hahahhahahah
+hahahhahahalpwjkewjh
+hahahhahahalpwjkewjhhhhhhhhhhhhhh
