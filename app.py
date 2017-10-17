a8b7b98ffe61139aac7d172fe025e0ad533ecfb6
lpj <pengjuli@gmail.com>
2017-10-17 01:47:57 -0700

    update

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from flask import render_template, redirect, url_for
 from libs import coroutine
 import threading
 import Queue
-from flask_socketio import SocketIO, emit, send
+from flask_socketio import SocketIO, emit, disconnect
 import subprocess
 app = Flask(__name__)
 app.debug = True
 
 socket_io = SocketIO(app)
+SHELL_PROCESS = None
+STREAM = None
 
 
 @app.route('/')
@@ -17,32 +19,53 @@ def hello_world():
     return render_template('index.html')
 
 
-@socket_io.on('show_log', namespace='/log')
-def show_log(msg):
-    r = subprocess.Popen("tail -f ~/Downloads/error.log", stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE, shell=True)
-    # t = threading.Thread(target=tail_file, args=(r, ))
-    t = ThreadExecuteJob(r)
-    t.start()
+@socket_io.on('connect', namespace='/log')
+def show_log():
+    print 'socket io connect'
+    try:
+        global SHELL_PROCESS, STREAM
+        SHELL_PROCESS = subprocess.Popen("tail -f ~/Downloads/error.log", stdout=subprocess.PIPE, shell=True)
+        # t = threading.Thread(target=tail_file, args=(r, ))
+        STREAM = SHELL_PROCESS.stdout
+        t = ThreadExecuteJob(STREAM)
+        # STREAM.readlines()
+        t.setDaemon(True)
+        t.start()
+    except Exception as e:
+        print 'error: ' + str(e.message)
 
 
-def tail_file(r):
-    while r.stdout.readline():
-        socket_io.emit('show_file_line', r.stdout.readline() + '<br/>', namespace='/log')
+@socket_io.on('disconnect')
+def socket_close():
+    print 'web close'
+    if STREAM is not None:
+        STREAM.close()
+
+    if SHELL_PROCESS is not None:
+        SHELL_PROCESS.terminate()
+    # disconnect()
+
+
+# def tail_file(stream):
+#     line = stream.readline()
+#     while line:
+#         socket_io.emit('show_file_line', line + '<br/>', namespace='/log')
+#         line = stream.readline()
 
 
 class ThreadExecuteJob(threading.Thread):
     """Threaded Url Grab"""
 
-    def __init__(self, r):
+    def __init__(self, stream):
         threading.Thread.__init__(self)
-        self.r = r
+        self.stream = stream
 
     def run(self):
-        while self.r.stdout.readline():
-            socket_io.emit('show_file_line', self.r.stdout.readline(), namespace='/log')
-
-
+        line = self.stream.readline()
+        while 1:
+            line = self.stream.readline()
+            if line:
+                socket_io.emit('show_file_line', line + '<br/>', namespace='/log')
 
 
 if __name__ == '__main__':
@@ -50,6 +73,3 @@ if __name__ == '__main__':
     # f = open('C:\\Users\\Administrator\\Desktop\\error.log')
     # gen_calling_obj = printer()
     # monitor_file(f, gen_calling_obj)
-hahahhahahah
-hahahhahahalpwjkewjh
-hahahhahahalpwjkewjhhhhhhhhhhhhhh
---
+++ test.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    # f = open('/home/cachecloud/Downloads/error.log', 'wa')
+    import time, datetime, logging
+    logging.basicConfig(
+        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+        level=logging.WARNING,
+        filename='/home/cachecloud/Downloads/error.log',
+        filemode='w'
+    )
+    count = 1
+    while 1:
+        count += 1
+        w_str = "this is mysql logging testing---" + str(count)
+        logging.warning(w_str + '\n')
+
+        time.sleep(5)
